09462fc065b88cbb6a44519da8d90b4da7186570
Necrozzyz <necrozzycontact@proton.me>
2025-12-13 22:23:47 -0300

    COnfiguração do HeaderController

--- app/Http/Controllers/HeaderController.php
+++ app/Http/Controllers/HeaderController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\HeaderAnalyzer;
+use App\Services\HeaderFetcher;
 
 class HeaderController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function analisar(
+        Request $request,
+        HeaderFetcher $fetcher,
+        HeaderAnalyzer $analyzer
+    )
+    {
+        $request->validate([
+            'url' => ['required','url']
+        ]);
+
+        $headers = $fetcher->fetch($request->url);
+
+        $resultado = $analyzer->analyze($headers);
+
+        return response()->json([
+            'url' => $request->url,
+            'resultado' => $resultado
+        ]);
+    }
 }
