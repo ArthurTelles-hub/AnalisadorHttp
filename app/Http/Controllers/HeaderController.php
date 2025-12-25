844a92899b7acd0890a4c6d687b53d392b24f2b0
Necrozzyz <necrozzycontact@proton.me>
2025-12-25 10:19:14 -0300

    Adicionando o modelo de analise ao controlador

--- app/Http/Controllers/HeaderController.php
+++ app/Http/Controllers/HeaderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\HeaderAnalyzer;
 use App\Services\HeaderFetcher;
+use App\Models\Analise;
 
 class HeaderController extends Controller
 {
@@ -31,5 +32,21 @@ class HeaderController extends Controller
             'url' => $request->url,
             'resultado' => $resultado
         ]);
+
+        $analise = Analise::create([
+            'url' => $request->url,
+            'status_code' => $resultado['status_code'],
+            'score' => $resultado['score'],
+            'nivel' => $resultado['nivel'],
+            'security_headers' => $resultado['security_headers'],
+            'cookies_inseguros' => $resultado['cookies']['inseguros'],
+        ]);
+    }
+
+    public function listar()
+    {
+        $analises = Analise::orderBy('created_at', 'desc')->get();
+
+        return responde()->json($analises);
     }
 }
