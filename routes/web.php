da69c09f7ed9068d24b2815020880d51f8c93b4e
Necrozzyz <necrozzycontact@proton.me>
2025-12-25 10:22:22 -0300

    Retirando a rota /analisar do web.php

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\HeaderController;
 
 Route::get('/', [HeaderController::class, 'index']);
 
-Route::get('/analisar', [HeaderController::class, 'analisar']);
+# Route::get('/analisar', [HeaderController::class, 'analisar']);
