cd9306e49d2aa1624637f47281c847ebbced9029
Necrozzyz <necrozzycontact@proton.me>
2025-12-25 10:23:19 -0300

    Adicionando rota responsavel pela listagem de cookies

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HeaderController;
 
 Route::post('/analisar', [HeaderController::class, 'analisar']);
+Route::get('/analises', [HeaderController::class, 'listar']);
