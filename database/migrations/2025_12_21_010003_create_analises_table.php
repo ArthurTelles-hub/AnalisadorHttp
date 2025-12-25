9750c753387e6e6a07f9cc81fdbac4a2f3680348
Necrozzyz <necrozzycontact@proton.me>
2025-12-25 10:23:59 -0300

    Criação da tabela no banco de dados para armazenamento

---
+++ database/migrations/2025_12_21_010003_create_analises_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('analises', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->integer('status_code')->nullable();
+            $table->integer('score');
+            $table->string('nivel');
+            $table->json('security_headers');
+            $table->json('cookies_inseguros')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('analises');
+    }
+};
